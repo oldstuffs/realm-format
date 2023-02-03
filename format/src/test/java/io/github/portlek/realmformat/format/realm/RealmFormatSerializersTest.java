8e879016dad528fbfa4ddac320c5e9def796a9b8
Hasan Demirtaş <utsukushihito@outlook.com>
2023-02-03 04:17:54 +0300

    Update RealmFormatSerializersTest.java

--- format/src/test/java/io/github/portlek/realmformat/format/realm/RealmFormatSerializersTest.java
+++ format/src/test/java/io/github/portlek/realmformat/format/realm/RealmFormatSerializersTest.java
@@ -5,25 +5,22 @@ import io.github.portlek.realmformat.format.realm.v1.RealmFormatWorldV1;
 import io.github.shiruka.nbt.Tag;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 final class RealmFormatSerializersTest {
 
   @Test
-  void deserialize() {
-  }
-
-  @Test
-  void serialize() throws IOException {
+  void test() throws IOException {
     final var original = RealmFormatWorldV1.builder()
       .extra(Tag.createCompound())
       .chunks(Collections.emptyMap())
-      .properties(new RealmFormatPropertyMap())
+      .properties(new RealmFormatPropertyMap(Tag.createCompound().setString("test", "test")))
       .worldVersion((byte) 1)
       .build();
     final var serialized = RealmFormatSerializers.serialize(original);
-    final var deserialized = RealmFormatSerializers.deserialize(serialized, new RealmFormatPropertyMap());
+    final var deserialized = RealmFormatSerializers.deserialize(serialized, new RealmFormatPropertyMap(Tag.createCompound().setString("test", "test")));
     Assertions.assertEquals(original, deserialized);
   }
 }
