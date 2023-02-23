7f0d5418781f816d09f43339d33fdd9de67f9b02
Hasan Demirtaş <utsukushihito@outlook.com>
2023-02-23 14:25:19 +0300

    add misc.

--- paper/nms/build.gradle.kts
+++ paper/nms/build.gradle.kts
@@ -1,6 +1,9 @@
 subprojects {
   dependencies {
+    compileOnly(project(":format"))
     compileOnly(project(":modifier:core"))
     compileOnly(project(":paper:api"))
+
+    compileOnly(rootProject.libs.nbt)
   }
 }
---
+++ paper/nms/v1_19_R2/src/main/java/io/github/portlek/realmformat/paper/nms/v1_19_R2/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package io.github.portlek.realmformat.paper.nms.v1_19_R2;
+
+import org.jetbrains.annotations.ApiStatus;
